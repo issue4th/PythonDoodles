8b25c412a5ebeaa67a9960a7e1271367ad33c50a
Rob Pym <rob_p@SLS-XPS15-RP.SHARP-LS.EU>
2019-11-05 19:39:50 +0000

    Bail out at sqrt of candidate. Separate list of possible factors for performance in loop. Larger check list (primes to 500)

--- .vscode/prime-finder-hackathon.py
+++ .vscode/prime-finder-hackathon.py
@@ -12,39 +12,44 @@ except:
 import matplotlib.pyplot as plt
 import numpy as np
 
-highest_number_to_test = 100000
+highest_number_to_test = 10000000
 
 def generate_primes_up_to(number):
-    primes = []
-    primes.append(3)
+    primes = [1, 2, 3]
+
+    possibleFactors = []
+    lastPossiblePrimeFactorIndex = 1
 
     loops = 0
     for backbone in range(6, number, 6):
         
         possiblePrime = int(backbone - 1)
-        if is_prime(possiblePrime, primes):
+        lastPossiblePrimeFactor = np.sqrt(possiblePrime)
+        while primes[lastPossiblePrimeFactorIndex] <= lastPossiblePrimeFactor:
+            possibleFactors.append(primes[lastPossiblePrimeFactorIndex])
+            lastPossiblePrimeFactorIndex += 1
+
+        if is_prime(possiblePrime, possibleFactors):
             primes.append(possiblePrime)
         
-        possiblePrime = int(backbone + 1)
-        if is_prime(possiblePrime, primes):
+        possiblePrime += 2
+        lastPossiblePrimeFactor = np.sqrt(possiblePrime)
+        while primes[lastPossiblePrimeFactorIndex] <= lastPossiblePrimeFactor:
+            possibleFactors.append(primes[lastPossiblePrimeFactorIndex])
+            lastPossiblePrimeFactorIndex += 1
+
+        if is_prime(possiblePrime, possibleFactors):
             primes.append(possiblePrime)
 
         loops += 1
         if loops % 100 == 0:
             print(backbone, len(primes) / backbone)
 
-    primes.insert(0, 2)
-    primes.insert(0, 1)
-
     return primes
 
-def is_prime(possiblePrime, possibleDivisors):
-    lastPossibleFactor = possiblePrime / 2 + 1
-
-    for possibleDivisor in possibleDivisors:
-        if possibleDivisor > lastPossibleFactor:
-            break
-        if possiblePrime % possibleDivisor == 0:
+def is_prime(possiblePrime, possibleFactors):
+    for possibleFactor in possibleFactors:
+        if possiblePrime % possibleFactor == 0:
             return False
 
     return True 
@@ -59,10 +64,20 @@ def map(func, listOfNumbers):
     return result
 
 def self_test():
-    checkPrimes = generate_primes_up_to(50)
-    if checkPrimes != [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]:
-        print("Broken!")
-        raise Exception("Self-test failed!")
+    to100 = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    to200 = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+    to300 = [211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293]
+    to400 = [307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397]
+    to500 = [401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499]
+    expectedPrimes = to100 + to200 + to300 + to400 + to500
+    
+    generatedPrimes = generate_primes_up_to(500)
+    if generatedPrimes != expectedPrimes:
+        print("Broken: ", generatedPrimes)
+        index = 0
+        while generatedPrimes[index] == expectedPrimes[index]:
+            index += 1
+        raise Exception("Self-test failed - mismatch at index ", index)
 
 self_test()
 primes = generate_primes_up_to(highest_number_to_test)
